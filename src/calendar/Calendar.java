63972f321be3a4c37d9beb6dc06ba80e413d04a7
citygray <nanee9244@gamail.com>
2023-03-07 07:04:04 +0900

    윤년 로직 구현

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,13 +1,25 @@
 package calendar;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Calendar {
 	private final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final int[] leapMaxDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	public boolean isLeapYear(int year) {
+		if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
-	public int getMaxDaysofMonth(int month) {
-		return maxDays[month - 1];
+	public int getMaxDaysofMonth(int year,int month) {
+		if(isLeapYear(year)) {
+			return leapMaxDays[month - 1];
+		}else {
+			return maxDays[month - 1];
+		}
 	}
 
 	public static void printSampleCalendar() {
@@ -19,11 +31,11 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 	}
 	
-	public static void printCalendar(int month) {
+	public static void printCalendar(int year,int month) {
 		Calendar calendar = new Calendar();
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
-		int maxDay = calendar.getMaxDaysofMonth(month);
+		int maxDay = calendar.getMaxDaysofMonth(year,month);
 		for (int i = 1; i < maxDay+1; i++) {
 			System.out.printf("%3d",i);
 			if(i%7==0) {
@@ -36,41 +48,28 @@ public class Calendar {
 	public static void main(String[] args) {
 		//가상의 달력 찍기
 		/* 요구사항 
-		 * 월을 입력하면 해당월의 달력을 출력한다.
-		 * 달력은 모양은 1단계에서 작성한 모양으로 만든다.
-		 * 1일은 일요일로 정해도 무방하다
-		 * -1을 입력받기 전까지 반복 입력받는다.
-		 * 프롬프트를 출력한다
+		 * 윤년을 적용한다.
 		 */
-		
-		/*입력 및 출력 예시
-		 	월을 입력하세요.
-			> 3
-			일 월 화 수 목 금 토
-			--------------------
-			 1  2  3  4  5  6  7
-			 8  9 10 11 12 13 14
-			15 16 17 18 19 20 21
-			22 23 24 25 26 27 28
-			29 30 31
-			월을 입력하세요.
-			> -1
-			Bye~
-		 */
-		String Promp = "cal> ";
+		String yearPromp = "year> ";
+		String monthPromp = "month> ";
 		Scanner scanner = new Scanner(System.in);
 		
 		while (true) {
+			System.out.println("년을 입력하세요.");
+			System.out.print(yearPromp);
+			int year = scanner.nextInt();
+			
 			System.out.println("월을 입력하세요.");
-			System.out.print(Promp);
+			System.out.print(monthPromp);
 			int month = scanner.nextInt();
-			if (month == -1) {
+			
+			if (month == -1 ) {
 				break;
 			}
 			if (month > 12) {
 				continue;
 			}
-			printCalendar(month);
+			printCalendar(year,month);
 
 		}
 		System.out.println("Bye~");
