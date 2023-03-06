b17148c6ddd8829e3d88328cbb5477232826ae0d
citygray <nanee9244@gamail.com>
2023-03-06 16:44:24 +0900

    가상의 달력 찍기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,6 @@
 package calendar;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Calendar {
@@ -17,21 +18,46 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
+	
+	public static void printCalendar(int month) {
+		Calendar calendar = new Calendar();
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("----------------------");
+		int maxDay = calendar.getMaxDaysofMonth(month);
+		for (int i = 1; i < maxDay+1; i++) {
+			System.out.printf("%3d",i);
+			if(i%7==0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
-		/*
-		 * 요구사항 
-		 * 전 단계와 비슷하게 반복입력을 받을 수 있게 구현한다. 
-		 * 입력하는 곳 앞에 프롬프트를 출력한다. 
-		 * -1일 입력받을 경우 프로그램을 종료한다.
+		//가상의 달력 찍기
+		/* 요구사항 
+		 * 월을 입력하면 해당월의 달력을 출력한다.
+		 * 달력은 모양은 1단계에서 작성한 모양으로 만든다.
+		 * 1일은 일요일로 정해도 무방하다
+		 * -1을 입력받기 전까지 반복 입력받는다.
+		 * 프롬프트를 출력한다
 		 */
-		/*
-		 * 입력 및 출력 예시 월을 입력하세요. > 3 3월은 31일까지 있습니다. 월을 입력하세요. > 2 2월은 28일까지 있습니다. 월을
-		 * 입력하세요. > -1 
-		 * Have a nice day!
+		
+		/*입력 및 출력 예시
+		 	월을 입력하세요.
+			> 3
+			일 월 화 수 목 금 토
+			--------------------
+			 1  2  3  4  5  6  7
+			 8  9 10 11 12 13 14
+			15 16 17 18 19 20 21
+			22 23 24 25 26 27 28
+			29 30 31
+			월을 입력하세요.
+			> -1
+			Bye~
 		 */
 		String Promp = "cal> ";
-		Calendar calendar = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 		
 		while (true) {
@@ -44,9 +70,10 @@ public class Calendar {
 			if (month > 12) {
 				continue;
 			}
-			System.out.printf("%d월의 최대일수는 %d입니다.%n", month, calendar.getMaxDaysofMonth(month));
+			printCalendar(month);
+
 		}
-		System.out.println("Have a nice day!");
+		System.out.println("Bye~");
 		scanner.close();
 	}
 
