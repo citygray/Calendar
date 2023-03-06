e30958f0d04ddbe4b3c7effb05249975635cc32d
citygray <nanee9244@gamail.com>
2023-03-06 15:31:51 +0900

    반복입력 가능하도록 수정

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,11 +3,12 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private final int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
-	
+	private final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysofMonth(int month) {
-		return maxDays[month-1];
+		return maxDays[month - 1];
 	}
+
 	public static void printSampleCalendar() {
 		System.out.println(" 일 월 화 수 목 금 토");
 		System.out.println("------------------");
@@ -18,16 +19,30 @@ public class Calendar {
 	}
 
 	public static void main(String[] args) {
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		System.out.println("월을 입력하시요");
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		/* 출력예시
+		 * 반복횟수를 입력하세요. 3 (엔터)
+		 * 
+		 * 월을 입력하세요. 1(엔터) 2(엔터) 3(엔터)
+		 * 
+		 * 1월은 31일까지 있습니다. 2월은 28일까지 있습니다. 3월은 31일까지 있습니다.
+		 */
+		
+		System.out.println("반복횟수를 입력하세요");
 		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
-		scanner.close();
-		
+		int count = scanner.nextInt();
+		int[] months = new int[count];
+
 		Calendar calendar = new Calendar();
-		System.out.printf("%d월의 최대일수는 %d입니다.%n",month,calendar.getMaxDaysofMonth(month));
 		
-		Calendar.printSampleCalendar();//클래스메서드
+		System.out.printf("월을 %d번 입력하세요%n",count);
+		for(var i = 0; i<months.length; i++) {
+			months[i] = scanner.nextInt();
+			System.out.printf("%d월의 최대일수는 %d입니다.%n", months[i], calendar.getMaxDaysofMonth(months[i]));
+		}
+		scanner.close();	
+
+	
 	}
 
 }
