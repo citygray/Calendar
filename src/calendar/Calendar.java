5507c3e952b81a290241116f0faa8750d0bad5f2
citygray <nanee9244@gamail.com>
2023-03-09 11:49:16 +0900

    - remove string if-else - remove 'Unreachable Code'

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,8 +1,50 @@
 package calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+
 public class Calendar {
 	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	private HashMap<Date, String> planMap;
+	
+	public Calendar() {
+		 planMap = new HashMap<Date, String>();
+	}
+	
+	/**
+	 * 
+	 * @param date ex:"2017-06-20"
+	 * @param plan
+	 * @throws ParseException
+	 */
+	public void registerPlan(String sDate, String plan) {
+		 // 문자열 -> Date
+		
+		try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(sDate);
+            planMap.put(date,plan);		
+            
+        } catch(ParseException e) {
+            e.printStackTrace();
+        }
+    }
+		
+	public String searchPlan(String sDate) {
+		try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(sDate);
+            String plan = planMap.get(date);		
+            return plan;
+            
+        } catch(ParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+	}
 
 	public boolean isLeapYear(int year) {
 		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -6,43 +6,107 @@ public class Prompt {
 	private final static String YEAR_PROMP = "year> ";
 	private final static String MONTH_PROMP = "month> ";
 
-	public void runPrompt() {
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+
+	}
+	/**
+	 * 1. Switch case - String
+	 * 2. PlanItem class 적용
+	 */
+
+	public void runPrompt(){
 		Scanner scanner = new Scanner(System.in);
 		Calendar calendar = new Calendar();
+		printMenu();
 
+		//Unreachable Code 처리
+		boolean isLoop = true;
+		while (isLoop) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scanner.next();
+			switch (cmd) {
+			case "1":
+					cmdRegister(scanner, calendar);
+					break;
+				case "2":
+					cmdSearch(scanner, calendar);
+					break;
+				case "3":
+					cmdCal(scanner, calendar);
+					break;
+				case "h":
+					printMenu();
+					break;
+				case "q": 
+					isLoop = false;
+					break;
+			}
+		}
+		System.out.println("Bye~");
+		scanner.close();
+	}
+
+	private void cmdCal(Scanner scanner, Calendar calendar) {
 		int month = 1;
 		int year = 0;
 		int weekDay = 0;
 
-		while (true) {
-			System.out.println("년을 입력하세요.(exit:-1)");
-			System.out.print(YEAR_PROMP);
-			year = scanner.nextInt();
-			if (year == -1) {
-				break;
-			}
+		System.out.println("년을 입력하세요.");
+		System.out.print(YEAR_PROMP);
+		year = scanner.nextInt();
 
-			System.out.println("월을 입력하세요.");
-			System.out.print(MONTH_PROMP);
-			month = scanner.nextInt();
+		System.out.println("월을 입력하세요.");
+		System.out.print(MONTH_PROMP);
+		month = scanner.nextInt();
 
-			// getStartWeekDay
-			weekDay = calendar.getStartWeekDay(year, month);
-
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-			calendar.printCalendar(year, month, weekDay);
-
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
 		}
-		System.out.println("Bye~");
-		scanner.close();
+
+		// getStartWeekDay
+		weekDay = calendar.getStartWeekDay(year, month);
+
+		calendar.printCalendar(year, month, weekDay);
+	}
+
+	private void cmdSearch(Scanner scanner, Calendar calendar) {
+		System.out.println("[일정 조회]");
+        System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+        String date = scanner.next();
+
+        System.out.println(calendar.searchPlan(date));
+		
+	}
+
+	private void cmdRegister(Scanner scanner, Calendar calendar) {
+		System.out.println("[새 일정 등록]");
+        System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+        String date = scanner.next();
+        String text = "";
+        scanner.nextLine(); //ignore one newline
+        System.out.println("일정을 입력해 주세요.");
+        text = scanner.nextLine();
+
+        calendar.registerPlan(date, text);
+		
 	}
 
 	public static void main(String[] args) {
 		// 셀 실행
 		Prompt p = new Prompt();
 		p.runPrompt();
+		
+		//test
+//		Calendar cal = new Calendar();
+//		cal.registerPlan("2023-03-12","일정등록 테스트");
+//		System.out.println(cal.searchPlan("2023-03-12").equals("일정등록 테스트"));
 	}
+
 }
